32dd8b6c5c79f63365999049bb811eccab8d4570
Diego Orozco <diego.orozco@ucr.ac.cr>
2019-06-07 17:59:58 -0600

    Se agrega el parametro para correrlo varias veces

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,14 +5,18 @@
 int main(int argc, char* argv[])
 {
 	int ret = 1;
-	if(argc >= 4)
+	if(argc >= 5)
 	{
-		Simulator simulator;
-		ret = simulator.run(argv);
+		int times = atoi(argv[4]);
+		for (int index = 0; index < times; ++index)
+		{
+			Simulator simulator;
+			ret = simulator.run(argv);			
+		}
 	}
 	else
 	{
-		std::cout << "Usage <Total time> <timeout> <delay>" << std::endl;
+		std::cout << "Usage <Total time> <timeout> <delay> <time>" << std::endl;
 	}
 
   	return ret;
